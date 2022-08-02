33e8610d9498a207cf74a8b4b5fe7f0953c5247e
step.twig@gmail.com <step.twig@gmail.com>
2022-08-02 16:23:59 +0000

    update Meme.js

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,11 +3,11 @@ import React from "react";
 export default function Meme() {
   return (
     <main className="">
-      <form>
-        <input />
-        <input />
-        <button>Get a new meme image</button>
-      </form>
+      <div className="form">
+        <input type="text" placeholder="Top text" className="form--input" />
+        <input type="text" placeholder="Bottom text" className="form--input" />
+        <button className="form--button">Get a new meme image</button>
+      </div>
     </main>
   );
 }
