c42efea94f4607245e198414bdfd37cf4360171b
step.twig@gmail.com <step.twig@gmail.com>
2022-08-02 15:52:37 +0000

    Create Meme.js component

---
+++ src/components/Meme.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Meme() {
+  return (
+    <main className="">
+      <form>
+        <input />
+        <input />
+        <button>Get a new meme image</button>
+      </form>
+    </main>
+  );
+}
