e09ff2321fc29e9e5e8c68ca09f96599df8566a6
step.twig@gmail.com <step.twig@gmail.com>
2022-08-02 20:43:06 +0000

    add onClick function to the button

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,12 +1,24 @@
 import React from "react";
+import memesData from "../memesData.js";
 
 export default function Meme() {
+  function getRandomImage() {
+    const memesArray = memesData.data.memes;
+    let randomNumber = Math.floor(Math.random() * memesArray.length);
+    console.log(randomNumber, "randdom number");
+
+    let randomImageUrl = memesData.data.memes[randomNumber].url;
+    console.log(randomImageUrl, "url");
+  }
+
   return (
     <main className="">
       <div className="form">
         <input type="text" placeholder="Top text" className="form--input" />
         <input type="text" placeholder="Bottom text" className="form--input" />
-        <button className="form--button">Get a new meme image</button>
+        <button className="form--button" onClick={getRandomImage}>
+          Get a new meme image
+        </button>
       </div>
     </main>
   );
